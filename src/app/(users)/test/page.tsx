b04d26e1bd14772703b4bd61680fdcefe3c75d20
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-06-10 14:18:27 +0700

    feat: Fix Button Components

---
+++ src/app/(users)/test/page.tsx
@@ -0,0 +1,18 @@
+import { FormButton, LinkButton } from "@/app/components/utils/Button";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <LinkButton href="#" variant="base" loading>
+        Hello
+      </LinkButton>
+      <LinkButton href="#" variant="white" loading>
+        Hello
+      </LinkButton>
+      <FormButton type="button" variant="disable">
+        Hai
+      </FormButton>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Navbar from "./components/utils/Navbar";
 import Image from "next/image";
-import LinkButton from "./components/utils/Button";
-
 
 export default function Home() {
   return (
