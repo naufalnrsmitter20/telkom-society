c596a55907ff8228784cba8c13b0557754d67676
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-06-10 13:47:49 +0700

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,5 @@ import Image from "next/image";
 import LinkButton from "./components/utils/Button";
 
 export default function Home() {
-  return (
-    <>
-      <LinkButton href="#" variant="white" className="mt-11 ml-6" withArrow={true}>
-        Join
-      </LinkButton>
-    </>
-  );
+  return <></>;
 }
