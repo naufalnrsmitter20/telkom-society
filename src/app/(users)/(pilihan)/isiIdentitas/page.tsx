e5bdd8d418a4dce427b86b76e389a3d69e8c1a62
phiniqq724 <fahrellnew@gmail.com>
2024-06-11 14:04:49 +0700

    chore: Update partner page and Navbar component

---
+++ src/app/(users)/(pilihan)/isiIdentitas/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function IsiIdentitas() {
+  return (
+    <main className="mt-[150px]">
+      <div className="">
+        <div>
+          <h1>Edit Profile</h1>
+          <p>Personal Data</p>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </main>
+  );
+}
