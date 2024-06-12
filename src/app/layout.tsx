028892b5ecb3277f8021b15f029cc77a4e4c67ee
Haza Nasrullah <hazanasrullah@gmail.com>
2024-06-12 09:48:22 +0700

    chore: Update InformasiPilihan page with improved image layout and content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,10 @@ import "./globals.css";
 import Navbar from "./components/utils/Navbar";
 import Footer from "./components/utils/Footer";
 
-const poppins = Poppins({ weight: "400", subsets: ["latin"] });
+const poppins = Poppins({ 
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], 
+  subsets: ["latin"] 
+});
 
 export const metadata: Metadata = {
   title: "Telkom Society",
