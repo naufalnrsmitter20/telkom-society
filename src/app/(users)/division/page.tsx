b95aa4bf0ae61eb2826c5a1ca9678a59ede6169a
phiniqq724 <fahrellnew@gmail.com>
2024-07-04 20:25:42 +0700

    chore: Update joinPage, Division, and IsiIdentitas page components

--- src/app/(users)/division/page.tsx
+++ src/app/(users)/division/page.tsx
@@ -1,3 +1,26 @@
+import { LinkButton } from "@/app/components/utils/Button";
+
 export default function Division() {
-  return <main>division</main>;
+  return (
+    <main className=" flex w-full flex-col h-screen ">
+      <h1 className="text-[48px] font-bold opacity-60 ml-[78px] mt-32">
+        Choose Between
+      </h1>
+      <div className="mx-auto w-fit my-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px] -mt-32">
+          <LinkButton
+            href="division/create"
+            variant="base"
+            className="scale-150"
+          >
+            Create
+          </LinkButton>
+          <h1 className="place-items-center flex ">Or</h1>
+          <LinkButton href="division/join" variant="base" className="scale-150">
+            Join
+          </LinkButton>
+        </div>
+      </div>
+    </main>
+  );
 }
