b95d5253da6451ea89e9e3b6c6deab10abe68561
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-07-23 14:43:46 +0700

    a

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { getToken } from "next-auth/jwt";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
+export default async function middleware(req: NextRequest, nex: NextFetchEvent) {
   const requireAuthToken = ["/partner", "/profile", "/profile/edit", "/isiIdentitas", "/isiIdentitas/personalData", "/isiIdentitas/achievement"];
   const pathname = req.nextUrl.pathname;
 
@@ -18,5 +18,5 @@ export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
 }
 
 export const config = {
-  matcher: ["/partner", "/profile", "/profile/edit", "/admin/:path*", "/admin", "/isiIdentitas", "/isiIdentitas/personalData", "/isiIdentitas/achievement"],
+  matcher: ["/admin/:path*", "/admin"],
 };
