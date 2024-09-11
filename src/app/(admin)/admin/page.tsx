d5f538ec27a3e4401acf2e4f802c0337a62192ae
GrowthToBetter <baratrahjaga@gmail.com>
2024-09-11 14:21:02 +0700

    add fitur notification, membuat page join dalam division dengan popup dan membuat api untuk mengambil database dari Team

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminHeaders from "./components/main/AdminHeaders";
+import { findAllUsers } from "@/utils/user.query";
+import { Prisma } from "@prisma/client";
 
 interface cardProps {
   title: string;
@@ -7,7 +9,11 @@ interface cardProps {
   desc: string;
 }
 
-export default function AdminPage() {
+export default async function AdminPage() {
+  const datas = await findAllUsers({
+    NOT: { role: "SISWA" },
+  });
+  
   const CardItem: cardProps[] = [
     {
       title: "Number of student",
@@ -31,17 +37,21 @@ export default function AdminPage() {
     },
   ];
   return (
-    <>
+    <div className="flex flex-col relative">
       <section className="w-full">
         <AdminHeaders data="Dashboard" />
         <section className="max-w-[1440px] mx-auto w-full bg-[#F6F6F6] p-4">
-          <h5 className="text-[24px] font-semibold text-[#F45846]">Statistik Data</h5>
+          <h5 className="text-[24px] font-semibold text-[#F45846]">
+            Statistik Data
+          </h5>
           <div className="grid grid-cols-4 p-4 gap-x-4">
             {CardItem.map((x, i) => (
               <div key={i} className="p-6 bg-white drop-shadow rounded-[12px]">
                 <p className="text-[16px] font-normal">{x.title}</p>
                 <div className="mt-6">
-                  <h6 className="text-[40px] font-medium text-[#F45846]">{x.data}</h6>
+                  <h6 className="text-[40px] font-medium text-[#F45846]">
+                    {x.data}
+                  </h6>
                   <p className="text-[14px] font-normal">{x.desc}</p>
                 </div>
               </div>
@@ -49,6 +59,21 @@ export default function AdminPage() {
           </div>
         </section>
       </section>
-    </>
+      <section className="w-full m-[10px]">
+        <section className="max-w-[1440px] mx-auto w-full bg-[#F6F6F6] p-4">
+          <h5 className="text-[24px] font-semibold text-[#F45846]">Admin</h5>
+          <div className="grid grid-cols-1 grid-rows-3 p-4 gap-y-4">
+            {datas.map((i,x) => (
+              <div key={x} className=" flex items-center justify-between p-6 bg-white drop-shadow rounded-[12px]">
+                <p className="text-[12px] font-normal mx-5">{i.name}</p>
+                <h6 className="text-[12px] font-normal mx-5">SMK Telkom Malang</h6>
+                <p className="text-[12px] font-normal mx-5">{i.email}</p>
+              </div>
+            ))}
+          </div>
+        </section>
+      </section>
+      <button className="w-[100px] h-[100px] text-white text-lg font-medium text-center items-center flex justify-center rounded-full fixed z-[10000] right-10 bottom-10 bg-[#F45846]"> add </button>
+    </div>
   );
 }
