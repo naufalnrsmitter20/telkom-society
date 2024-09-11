bd1fe14bcf162447ef7482c4fb738bb6eb1bc89f
GrowthToBetter <baratrahjaga@gmail.com>
2024-09-11 19:28:11 +0700

    membuat dashboard, list  student, dan achievment di page admin agar dapat mengubah role pada database

--- src/app/(admin)/admin/achievementData/page.tsx
+++ src/app/(admin)/admin/achievementData/page.tsx
@@ -2,10 +2,35 @@ import React from "react";
 import AdminHeaders from "../components/main/AdminHeaders";
 
 export default function achievementData() {
-  return (
-    <>
-      <AdminHeaders data="Achievement Data" />
-      <div></div>
-    </>
+  return (<>
+    <AdminHeaders data="Achievement Data" />
+    <div className="flex flex-col w-screen justify-center items-center h-screen">
+      <div className="bg-secondary flex justify-center rounded-xl w-3/4 h-1/2 mt-28">
+        <div className="flex justify-between">
+          <div className="m-5">
+            <h1 className="m-2">Ranking Siswa</h1>
+            <ul>
+              <li>1 .Naufal Nabil Ramadhan (300.000 point)</li>
+              <li>2. Naufal Nabil Ramadhan (300.000 point)</li>
+              <li>3. Naufal Nabil Ramadhan (300.000 point)</li>
+              <li>4. Jean Richnerd Rantabaratrahjaga (300.000 point)</li>
+              <li>5. Jean Richnerd Rantabaratrahjaga (300.000 point)</li>
+            </ul>
+          </div>
+          <div className="m-5">
+            <h1 className="m-2">Ranking Guru</h1>
+            <ul>
+              <li>1. Muhammad Chusni Agus (300.000 point)</li>
+              <li>2. Muhammad Chusni Agus (300.000 point)</li>
+              <li>3. Muhammad Chusni Agus (300.000 point)</li>
+              <li>4. Muhammad Chusni Agus (300.000 point)</li>
+              <li>5. Muhammad Chusni Agus (300.000 point)</li>
+            </ul>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </>
   );
 }
--- src/app/(admin)/admin/studentData/page.tsx
+++ src/app/(admin)/admin/studentData/page.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+
+
+import React, { ChangeEvent } from "react";
+import { findAllUsers } from "@/utils/user.query";
+import {
+  updateRole,
+} from "@/utils/server-action/userGetServerSession";
+import toast from "react-hot-toast";
+import Hero from "@/app/(admin)/admin/components/Hero/page";
 import AdminHeaders from "../components/main/AdminHeaders";
 
-export default function studentData() {
+
+export default async function studentData() {
+  const datas = await findAllUsers({
+    role: "SISWA",
+  });
+
+  
   return (
-    <>
+    <div className="flex flex-col">
       <AdminHeaders data="Student Data" />
-      <div></div>
-    </>
+      <Hero datas={datas}/>
+    </div>
   );
 }
